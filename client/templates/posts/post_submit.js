2789ff2c6cf60e3a0c508b161542c0f7f169106b
Alexander Thomsen <abw.thomsen@gmail.com>
2018-03-11 16:51:19 +0300

    Commit 7-1
    Added a submit post page and linked to it in the header.

---
+++ client/templates/posts/post_submit.js
@@ -0,0 +1,14 @@
+// Event handler to take submit form
+Template.postSubmit.events({
+  'submit form': function(e) {
+    e.preventDefault();
+
+    var post = {
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val()
+    };
+
+    post._id = Posts.insert(post);
+    Router.go('postPage', post);
+  }
+});
